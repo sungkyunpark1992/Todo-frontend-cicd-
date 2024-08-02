6d474b5a9194f4ef1982abc6afa1ba06d20af68f
sungkyunpark1992 <qkrtjdrbs125@naver.com>
2024-08-02 05:34:59 +0000

    frontend deployment

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else{
-  backendHost = "http://todoapplication00-backend-dev222.ap-northeast-2.elasticbeanstalk.com";
+  backendHost = "dev.psktest.p-e.kr";
 }
 
 export const API_BASE_URL = `${backendHost}`;
